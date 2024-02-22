d3e6f82bac9a7eaef1ae05b6343163579a697eb5
Marina Rachlin <Marina.rachlin@gmail.com>
2024-02-22 13:55:53 +0200

    changes

--- app/page.js
+++ app/page.js
@@ -12,8 +12,13 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <Head>
-       {''}
+       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>"Hydro-Plan IL"</title>
+        <description>"Hydro-Plan IL, עופרי אורגד"</description>
+        <meta name="description" content="יעוץ הידרולוגי לתב''ע, נספח ניקוז, ניהול נגר להיתר, ניקוד בנייה ירוקה, פיקוח עליון לטופס 4" />
+        <meta name="keywords"content='hydro-plan, עופרי אורגד, נספח ניקוז ,פרשה טכנית עופרי ,ניהול נגר ,בנייה ירוקה  ,תמ׳׳א 62 ,תמ"א 1, פיקוח עליון, טופס 4, דוח הידרולוג, הצהרת הידרולוג, היתר, תב"ע, מנהל התכנון, רשות המים'/>
       </Head>
       <Script
           async
